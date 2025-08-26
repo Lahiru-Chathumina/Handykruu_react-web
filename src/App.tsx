33bd2b892a5a5ab2e94943c10f3133492aefed53
Lahiru-Bandara <chathuminabandaralahiru@gmail.com>
2025-08-26 10:44:36 +0530

    update all

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from './components/Header.tsx';
+import Hero from './components/Hero.tsx';
+import Services from './components/Services.tsx';
+import About from './components/About.tsx';
+import Portfolio from './components/Portfolio.tsx';
+import Testimonials from './components/Testimonials.tsx';
+import Contact from './components/Contact.tsx';
+import Footer from './components/Footer.tsx';
+import './index.css' 
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      <Services />
+      <About />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
